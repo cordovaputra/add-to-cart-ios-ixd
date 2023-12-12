8798a4fffb846c670cdd92d2bae2c1eb59d40822
cordovaputra <cordovaputra.handri@gmail.com>
2023-12-12 16:49:46 +0700

    Initial Commit

---
+++ Add-to-Cart-IXD/Add_to_Cart_IXDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Add_to_Cart_IXDApp.swift
+//  Add-to-Cart-IXD
+//
+//  Created by Cordova  on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Add_to_Cart_IXDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
